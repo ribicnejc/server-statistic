259dc6e739c995b685d920a4a709860aa014bcc9
Nejc Ribic <nejc.ribic95@gmail.com>
2019-02-05 16:51:14 +0100

    Added migration scripts

--- src/database/table-migration-1-2019-02-05.sql
+++ src/database/table-migration-1-2019-02-05.sql
@@ -1,5 +1,4 @@
 ALTER TABLE `test_application`
   ADD (`LAT` float,
        `LNG` float,
-       `IMG_URL` text COLLATE utf8_bin,
-       `HOT` BOOL NULL);
+       `META_DATA` text COLLATE utf8_bin);
